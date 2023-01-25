c086566c6726d4e8c41dd3cea0bf7df88872f93d
daniela guzman <danielaguzman50@outlook.es>
2023-01-24 21:06:21 -0500

    added map

--- src/components/Dashboard/Places/New.jsx
+++ src/components/Dashboard/Places/New.jsx
@@ -1,9 +1,37 @@
+import { Wrapper } from "@googlemaps/react-wrapper";
+import React, { useRef, useState, useEffect } from "react";
 import "./New.scss";
+
+const API_KEY = "AIzaSyCEZrA2-qiOdhZhtyOFJvosWAMaX4MbyuQ";
+
+const render = (status) => {
+  return <h1>{status}</h1>;
+};
+
+function Map(props) {
+  const ref = useRef(null);
+  const [map, setMap] = useState();
+  useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+  useEffect(() => {
+    if (map) {
+      map.setOptions({
+        center: props.center,
+        zoom: props.zoom,
+      });
+    }
+  }, [map, props.center, props.zoom]);
+  return <div ref={ref} style={props.style} />;
+}
 
 function Category() {
   return (
     <div className="Category">
-      <label for="category" className="Category__label">
+      <label htmlFor="category" className="Category__label">
         Categoria
       </label>
       <select name="category" className="Category__select">
@@ -27,12 +55,24 @@ function Category() {
 function PlaceForm() {
   return (
     <form className="PlaceForm">
-      <label for="name" className="PlaceForm__label">
+      <label htmlFor="name" className="PlaceForm__label">
         Nombre
       </label>
       <input type="text" name="name" className="PlaceForm__input" />
       <div className="PlaceForm__category">
         <Category></Category>
+      </div>
+      <div style={{ height: "400px", width: "100%" }}>
+        <Wrapper apiKey={API_KEY} render={render}>
+          <Map
+            style={{ height: "100%" }}
+            zoom={12}
+            center={{
+              lat: 5.467467597038992,
+              lng: -74.67453033896683,
+            }}
+          ></Map>
+        </Wrapper>
       </div>
       <input type="submit" value="agregar" className="PlaceForm__submit" />
     </form>
